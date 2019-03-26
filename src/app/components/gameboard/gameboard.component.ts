70201d78eb7691d5f1bfc705c1212cdbaa25a9fa
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2019-03-26 22:07:57 +0100

    Add gameboard constructor

--- src/app/components/gameboard/gameboard.component.ts
+++ src/app/components/gameboard/gameboard.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gameboard',
   templateUrl: './gameboard.component.html',
   styleUrls: ['./gameboard.component.scss']
 })
-export class GameboardComponent implements OnInit {
+export class GameboardComponent {
 
-  constructor() { }
+  boardSize = 2;
+  tiles = [];
 
-  ngOnInit() {
+  constructor() {
+    var row;
+    for(var i = 0; i < this.boardSize; i++) {
+      row = []
+      for(var j = 0; j < this.boardSize; j++) {
+        row.push(1);
+      }
+      this.tiles.push(row);
+    }
   }
-
-
-  tiles = [
-    [1, 2],
-    [1, 2],
-  ];
-
 }
