a4651af43151bbc289093049a194e2d4d5a2dcc6
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2019-04-03 20:39:16 +0200

    Fix runtime issue for programs with only 1 instruction

--- src/app/models/program.ts
+++ src/app/models/program.ts
@@ -1,21 +1,33 @@
 import { Instruction } from './instruction';
 import Utils from 'app/utils';
 
+export enum State {
+  running, stopped
+}
+
 export class Program {
   instructions: Instruction[];
   runtime: any;
+  state: State;
 
   constructor() {
     this.instructions = [];
+    this.state = State.stopped;
   }
   run(tick, tickRate) {
+    this.state = State.running;
     tick();
-    this.runtime = setInterval(() => {
-      tick();
-    }, tickRate);
+    if (this.state === State.running) {
+      this.runtime = setInterval(() => {
+        tick();
+      }, tickRate);
+    }
   }
   stop() {
-    clearInterval(this.runtime);
+    if (this.runtime) {
+      clearInterval(this.runtime);
+    }
+    this.state = State.stopped;
   }
   copy() {
     const p = new Program();
